6d2889571cffff3a4cc6f65363d3ea18fdb13041
kryptokommunist <marcusding@me.com>
2015-02-10 20:11:54 +0100

    New nyancat

--- png.c
+++ png.c
@@ -3,10 +3,10 @@
 #include "lodepng.h"
 #include <stdlib.h>
 #include <unistd.h>
-const int chann = 1;
-const int frequ = 1000000;
-const int leds = 480;
-const int buffsize = leds * 3;
+
+#define SPI_FREQUENCY   1000000
+#define SPI_CHANNEL     1
+
 #define CRATE_WIDTH     5
 #define CRATE_HEIGHT    4
 #define CRATES_X        6
@@ -14,17 +14,17 @@ const int buffsize = leds * 3;
 #define BUS_COUNT       4
 #define BYTES_PER_PIXEL 3
 #define CRATES_PER_BUS  6
-#define BUS_ROWS                (CRATES_Y*CRATE_HEIGHT)
-#define CRATE_COUNT             (CRATES_X*CRATES_Y)
-#define CRATE_SIZE              (CRATE_WIDTH*CRATE_HEIGHT)
-#define BUS_SIZE                (CRATES_PER_BUS*CRATE_SIZE*BYTES_PER_PIXEL)
-#define BUFF_SIZE               (CRATE_COUNT*CRATE_SIZE*BYTES_PER_PIXEL)
+#define BUS_ROWS    (CRATES_Y*CRATE_HEIGHT)
+#define CRATE_COUNT   (CRATES_X*CRATES_Y)
+#define CRATE_SIZE    (CRATE_WIDTH*CRATE_HEIGHT)
+#define BUS_SIZE    (CRATES_PER_BUS*CRATE_SIZE*BYTES_PER_PIXEL)
+#define BUFF_SIZE                (CRATE_COUNT*CRATE_SIZE*BYTES_PER_PIXEL)
 
 unsigned const char BOTTLE_MAP[CRATE_SIZE] = {
-           6, 7,  8, 15, 16, 
-           5, 4,  9, 14, 17, 
-           0, 3, 10, 13, 18, 
-           1, 2, 11, 12, 19
+     10, 15,  16, 11, 6, 
+     5, 0,  1, 2, 7, 
+     12, 17, 18, 13, 8, 
+     3, 4, 9, 14, 19
 };
 
 
@@ -33,15 +33,15 @@ void decodeOneStep(const char* filename);
 
 int main() {
 
-if(wiringPiSPISetup(chann, frequ) == -1) {
+if(wiringPiSPISetup(SPI_CHANNEL, SPI_FREQUENCY) == -1) {
 
         printf("Could not initialize SPI!\n");
 
 }
 
-unsigned char buff[buffsize];
+unsigned char buff[BUFF_SIZE];
 
-for(int i = 0; i < buffsize; i++){
+for(int i = 0; i < BUFF_SIZE ; i++){
 
 buff[i] = 0xFF;
 
@@ -49,31 +49,39 @@ buff[i] = 0xFF;
 
 printf("Came to the end\n");
 
-wiringPiSPIDataRW(chann,buff,buffsize);
+wiringPiSPIDataRW(SPI_CHANNEL,buff,BUFF_SIZE);
 
 while(true) {
-wiringPiSPIDataRW(chann,buff,buffsize);
-sleep(2);
+wiringPiSPIDataRW(SPI_CHANNEL,buff,BUFF_SIZE);
+sleep(3);
 decodeOneStep("nyancat.png");
-sleep(2);
+sleep(3);
 decodeOneStep("rgb.png");
-sleep(2);
+sleep(3);
 decodeOneStep("hack.png");
-sleep(2);
+sleep(3);
 decodeOneStep("circle.png");
-sleep(2);
+sleep(3);
 decodeOneStep("checker.png");
-sleep(2);
+sleep(3);
 decodeOneStep("green-checker.png");
-sleep(2);
+sleep(3);
+decodeOneStep("red-line.png");
+sleep(3);
+decodeOneStep("line.png");
+sleep(3);
+decodeOneStep("test.png");
+sleep(3);
 decodeOneStep("wiki.png");
 sleep(2);
 decodeOneStep("chinese.png");
 sleep(2);
 decodeOneStep("ok.png");
+sleep(2);
 }
 
 }
+
 
 void decodeOneStep(const char* filename)
 {
@@ -88,7 +96,7 @@ void decodeOneStep(const char* filename)
   int buffcnt = 0;
   for(int i = 0; i < (CRATE_COUNT * CRATE_SIZE * 4); i++){
 
-        if(i==0 ||i % 4 != 0) {buffer[buffcnt] = image[i]; buffcnt++; /* printf("%x\n", image[i]);*/}
+  if(i==0 ||i % 4 != 0) {buffer[buffcnt] = image[i]; buffcnt++; /* printf("%x\n", image[i]);*/}
 
    }
 
@@ -104,9 +112,8 @@ for(int bottle_x = 0; bottle_x < CRATE_WIDTH*3; bottle_x++){
 
 for(int bottle_y = 0; bottle_y < CRATE_HEIGHT; bottle_y++) {
 
-        cratesData[crate_y * CRATES_X + crate_x][bottle_y * CRATE_WIDTH * 3 + bottle_x] = buffer[crate_y * CRATE_SIZE*CRATES_X*3 + crate_x * CRATE_WIDTH*3 + bottle_y * CRATE_WIDTH * CRATES_X*3 + bottle_x];
-                   
-  
+  cratesData[crate_y * CRATES_X + crate_x][bottle_y * CRATE_WIDTH * 3 + bottle_x] = buffer[crate_y * CRATE_SIZE*CRATES_X*3 + crate_x * CRATE_WIDTH*3 + bottle_y * CRATE_WIDTH * CRATES_X*3 + bottle_x];
+
 }
 
 }
@@ -131,10 +138,7 @@ for(int k = 0; k < 3; k++) {
 }
 
   /*use image here*/
-  wiringPiSPIDataRW(chann,buffer,BUFF_SIZE);
-  printf("Width %i\n", width);printf("Height %i\n", height);
+  wiringPiSPIDataRW(SPI_CHANNEL,buffer,BUFF_SIZE);
+  /*printf("Width %i\n", width);printf("Height %i\n", height);*/
 
 }
-
-
-
