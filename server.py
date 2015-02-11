c9de151153cac08859e3ad1d2ca58b4f5476575e
kryptokommunist <marcusding@me.com>
2015-02-12 00:06:25 +0100

    server added

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import ctypes
 display = ctypes.CDLL("/home/pi/matepi/matelight-gifplayer-master/display.so")
 
 HOST = "192.168.2.157"
-PORT = 1377
+PORT = 1337
 BUFFSIZE = 16*30*20*3
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -14,5 +14,6 @@ s.bind((HOST,PORT))
 print "hunny, i'm listening..."
 
 while 1:
-  data = (ctypes.c_ubyte * BUFFSIZE) (*s.recv(BUFFSIZE))
+  data = (ctypes.c_ubyte * BUFFSIZE) (*[x for sets in s.recv(BUFFSIZE) for x in sets])
   display.display(ctypes.cast(data, ctypes.POINTER(ctypes.c_ubyte)))
+  print "received data"
