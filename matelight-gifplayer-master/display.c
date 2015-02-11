87f506a55082ee61209907033ed8ebf3d76c4f44
kryptokommunist <marcusding@me.com>
2015-02-11 21:04:13 +0100

    Bugfix in Gifplayer

--- matelight-gifplayer-master/display.c
+++ matelight-gifplayer-master/display.c
@@ -26,44 +26,45 @@ unsigned const char BOTTLE_MAP[CRATE_SIZE] = {
 	   3, 4, 9, 14, 19
 };
 
+int spi_initialized = 0;
+
 void display(unsigned char data[BUFF_SIZE]);
 
-void init_spi();
-
-void init_spi()
-{
-
-	if(wiringPiSPISetup(SPI_CHANNEL, SPI_FREQUENCY) == -1) {
-
-        printf("Could not initialize SPI!\n");
-
-    }
-
-    printf("I just initalized SPI, bitch!\n");
-
-}
 
 /* Takes filename, return buffer containing image data. Length ist BUFF_SIZE*/
 void display(unsigned char data[BUFF_SIZE])
 {
 
+if(!spi_initialized) {
+
+	if(wiringPiSPISetup(SPI_CHANNEL, SPI_FREQUENCY) == -1) {
+
+	        printf("Could not initialize SPI!\n");
+
+	    }
+
+	printf("I just initalized SPI, bitch!\n");
+
+	spi_initialized = -1;
+}
+
 unsigned char cratesData[CRATE_COUNT][CRATE_SIZE * 3];
 
 for(int crate_x = 0; crate_x < CRATES_X; crate_x++){
 
-for(int crate_y = 0; crate_y < CRATES_Y; crate_y++){
+	for(int crate_y = 0; crate_y < CRATES_Y; crate_y++){
 
-for(int bottle_x = 0; bottle_x < CRATE_WIDTH*3; bottle_x++){
+		for(int bottle_x = 0; bottle_x < CRATE_WIDTH*3; bottle_x++){
 
-for(int bottle_y = 0; bottle_y < CRATE_HEIGHT; bottle_y++) {
+			for(int bottle_y = 0; bottle_y < CRATE_HEIGHT; bottle_y++) {
 
-	cratesData[crate_y * CRATES_X + crate_x][bottle_y * CRATE_WIDTH * 3 + bottle_x] = data[crate_y * CRATE_SIZE*CRATES_X*3 + crate_x * CRATE_WIDTH*3 + bottle_y * CRATE_WIDTH * CRATES_X*3 + bottle_x];
+				cratesData[crate_y * CRATES_X + crate_x][bottle_y * CRATE_WIDTH * 3 + bottle_x] = data[crate_y * CRATE_SIZE*CRATES_X*3 + crate_x * CRATE_WIDTH*3 + bottle_y * CRATE_WIDTH * CRATES_X*3 + bottle_x];
 
-}
+			}
 
-}
+		}
 
-}
+	}
 
 }
 
@@ -71,19 +72,19 @@ unsigned char buffer[BUFF_SIZE];
 
 for(int i = 0; i < CRATE_COUNT; i++){
 
-for(int j = 0; j < CRATE_SIZE; j++) {
+	for(int j = 0; j < CRATE_SIZE; j++) {
 
-for(int k = 0; k < 3; k++) {
+		for(int k = 0; k < 3; k++) {
 
-   buffer[i*CRATE_SIZE*3+j*3+k] = cratesData[i][BOTTLE_MAP[j]*3+k];
+		   buffer[i*CRATE_SIZE*3+j*3+k] = cratesData[i][BOTTLE_MAP[j]*3+k];
 
-}
-
-}
+		}
 
 }
 
-        wiringPiSPIDataRW(SPI_CHANNEL,buffer,BUFF_SIZE);
+}
+
+wiringPiSPIDataRW(SPI_CHANNEL,buffer,BUFF_SIZE);
 
 
 
