fe91390cfd78bb4c6b282013f6437e36a7f06a87
kryptokommunist <marcusding@me.com>
2015-02-15 00:31:01 +0100

    modification

--- server.py
+++ server.py
@@ -35,7 +35,7 @@ def sendframe(framedata):
     # just use the first Mate Light available
     rgba = len(framedata) == DISPLAY_WIDTH*DISPLAY_HEIGHT*4
     global dbuf
-    numpy.copyto(dbuf[:480*(3+rgba)], numpy.frombuffer(framedata, dtype=numpy.uint8))
+    numpy.copy(dbuf[:480*(3+rgba)], numpy.frombuffer(framedata, dtype=numpy.uint8))
     display.display(dbuf.ctypes.data_as(POINTER(c_uint8)))		
 
 
