31dbfcbc41bc8454171f231198a3c8eb97ca66d1
kryptokommunist <marcusding@me.com>
2015-02-15 14:44:45 +0100

    modification

---
+++ aus.py
@@ -0,0 +1,17 @@
+from socketserver import *
+import socket
+import struct
+import zlib
+from time import time, strftime, sleep
+from collections import namedtuple, deque
+import itertools
+import threading
+import random
+import os
+
+from ctypes import *
+
+from server import sendframe, DISPLAY_WIDTH, DISPLAY_HEIGHT, FRAME_SIZE
+
+if __name__ == '__main__':
+    sendframe(bytes([0, 0, 0]*FRAME_SIZE))
