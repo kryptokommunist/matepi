492c73958c6b746652539556c690011fdd8c4dce
kryptokommunist <marcusding@me.com>
2015-05-20 01:04:23 +0200

    modification

--- server.py
+++ server.py
@@ -98,8 +98,8 @@ def sendframe(framedata):
     rgba = len(framedata) == DISPLAY_WIDTH*DISPLAY_HEIGHT*4
 
     if rgba == 1:
-       framedata = numpy.frombuffer(framedata, dtype=numpy.uint8)
-       framedata = numpy.delete(framedata, numpy.arange(0, framedata.size, 4))
+       #framedata = numpy.frombuffer(framedata, dtype=numpy.uint8)
+       #framedata = numpy.delete(framedata, numpy.arange(0, framedata.size, 4))
 
     global dbuf
     numpy.copyto(dbuf[:480*(3+rgba)], numpy.frombuffer(framedata, dtype=numpy.uint8))
