59258711f309b2f8cc8dcbd76e50b30062a348d0
kryptokommunist <marcusding@me.com>
2015-05-22 02:36:16 +0200

    modification

--- server4.py
+++ server4.py
@@ -130,7 +130,7 @@ class UDPServer:
   def __iter__(self):
     while True:
       with self.frame_condition:
-        if not self.frame_condition.wait(timeout=UDP_TIMEOUT) and self.frame_da:
+        if not self.frame_da and self.frame_condition.wait(timeout=UDP_TIMEOUT):
           raise StopIteration()
         frame, self.frame = self.frame, None
         yield frame
