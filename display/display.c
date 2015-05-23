bbc6ccfb931a32cfaac2f893c9017f3397309147
kryptokommunist <marcusding@me.com>
2015-05-23 19:49:29 +0200

    modification

--- display/display.c
+++ display/display.c
@@ -138,6 +138,6 @@ for(int i = 0; i < CRATE_COUNT; i++){
 }
 
 wiringPiSPIDataRW(SPI_CHANNEL,buffer,BUFF_SIZE);
-sleep(0.2); // give time to display, without this the data somehow doesn't get pushed to the display...
+sleep(0.4); // give time to display, without this the data somehow doesn't get pushed to the display...
 
 }
